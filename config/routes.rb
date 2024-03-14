647fc1ae1b01428af61a1fae183d57a373c57476
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-03-13 21:06:21 -0500

    added testing
    still getting two failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
-get '/users', to: 'users#index'
+# get '/users', to: 'users#index'
 
-get '/users/:id', to: 'users#show'
+# get '/users/:id', to: 'users#show'
 
-post '/users', to: 'users#create'
+# post '/users', to: 'users#create'
+
+# put '/users/:id', to: 'users#update'
+
+# delete '/users/:id', to: 'users#destroy'
+
+resources :users do
+  get 'posts', to: "users#posts_index"
+  end
+
+  resources :posts
 end
-
-
-
